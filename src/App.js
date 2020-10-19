c68ea1e3d90c23373c14b6cd6d6350f9e3c8256f
Caio Freitas <caiof@take.net>
2020-10-19 09:33:37 -0300

    Add filter form

--- src/App.js
+++ src/App.js
@@ -4,47 +4,42 @@ import { PageHeader } from "components/PageHeader"
 import { CommonProvider } from "contexts/CommonContext"
 import { PageTemplate } from "components/PageTemplate"
 import { getOpenTickets, closeTicket } from "api/axiosService"
-import { Button, Form, Col, Row, ProgressBar } from "react-bootstrap"
-import { BlipTable } from "components/BlipTable";
-import { sortData } from './util';
+import { ProgressBar } from "react-bootstrap"
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ItemModal from './projectComponents/ItemModal'
+import CommandForm from './projectComponents/CommandForm'
+import DataTable from './projectComponents/DataTable'
 
 
-const tableModel = [
-    { label: "Sequential Id", key: "sequentialId" },
-    { label: "Customer Identity", key: "customerIdentity" },
-    { label: "Team", key: "team" },
-    { label: "Open Date", key: "openDate" },
-    { label: "Last Client Message Date", key: "lastMessageDate" },
-];
+
 
 
 function App() {
 
-    const [key, setkey] = useState('');
-    const [url, setUrl] = useState('');
+    const [header, setHeader] = useState({});
     const [data, setData] = useState([]);
     const [selected, setSeleted] = useState([]);
     const [progress, setProgress] = useState();
     const [percentage, setPercentage] = useState(0);
     const [modal, setModal] = useState({ position: 0, display: false, item: {} });
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = async (event, header) => {
+        console.log(header)
         event.preventDefault();
-        loadData();
+        loadData(header);
     }
 
 
 
 
-    const loadData = async () => {
+    const loadData = async (header) => {
+        setHeader(header)
         setData([]);
         setSeleted([]);
         setProgress({ visibility: true })
         setPercentage(0)
-        setData(await getOpenTickets(key, url, handleError, setPercentage))
+        setData(await getOpenTickets(header, handleError, setPercentage))
         setProgress({ visibility: false })
         setPercentage(0)
     }
@@ -59,12 +54,12 @@ function App() {
         let successNumber = 0;
 
         for (const item of selected) {
-            if (await closeTicket(key, url, item.id, handleError)) {
+            if (await closeTicket(header.key, header.url, item.id, handleError)) {
                 successNumber++
             }
         }
         successNumber > 0 && toast.success(`${successNumber} ticket(s) closed`);
-        await loadData();
+        await loadData(header);
     }
 
 
@@ -89,43 +84,21 @@ function App() {
                 <PageTemplate title={title}>
                     <div id="tab-nav" >
                         <ItemModal position={modal.position} display={modal.display} data={modal.item} handleClose={() => setModal({ ...modal, display: false })} />
-                        <Form onSubmit={handleSubmit}>
-                            <Form.Group as={Row} >
-                                <Form.Label column sm="3">Url to send commands</Form.Label>
-                                <Col sm="9">
-                                    <Form.Control type="text" required placeholder="https://http.msging.net/commands" value={url} onChange={(e) => { setUrl(e.target.value) }} /><br />
-                                </Col>
 
-                                <Form.Label column sm="3">Header authentication (Authorization)</Form.Label>
-                                <Col sm="9">
-                                    <Form.Control type="text" required placeholder="Key bGFiqpolfyaW9u..." value={key} onChange={(e) => { setkey(e.target.value) }} />
-                                </Col>
+                        <CommandForm handleSubmit={handleSubmit} />
 
-                            </Form.Group>
-                            <Button className="float-right" type="submit" >Load</Button>
-                        </Form>
                         <div style={progress && progress.visibility ? { display: '' } : { display: "none" }}>
                             <ProgressBar now={percentage} label={`${percentage}%`} />
                         </div>
-                        <div style={progress && !progress.visibility ? { visibility: '' } : { visibility: 'hidden' }} className="tickets-box">
 
-                            <h3>Open Tickets</h3>
-                            <p> Click on tickets to see their information </p>
-
-                            <BlipTable
-                                idKey="id"
-                                model={tableModel}
-                                data={data}
-                                canSelect={true}
-                                sort={{}}
-                                onSortSet={(item) => { sortData(data, item) }}
-                                onItemClick={(event, item) => { setModal({ position: event.nativeEvent.clientY, display: true, item: item }); }}
-                                onItemSelect={(item) => setSeleted(item)}
-                                selectedItems={selected}
-                                bodyHeight="1300px"
-                                actions={[<Button key='123' variant="danger" onClick={handleClosing}>Close</Button>]}
-                            />
-                        </div>
+                        <DataTable
+                            data={data}
+                            handleClosing={handleClosing}
+                            progress={progress}
+                            selected={selected}
+                            setSeleted={setSeleted}
+                            setModal={setModal}
+                        />
                     </div>
 
                 </PageTemplate>
