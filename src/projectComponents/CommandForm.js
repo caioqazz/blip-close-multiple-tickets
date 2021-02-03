ac578d1113cabff14c1285ff0c6925eb847a190b
Caio Freitas <caiof@take.net>
2021-02-03 14:39:32 -0300

    Fix url

--- src/projectComponents/CommandForm.js
+++ src/projectComponents/CommandForm.js
@@ -22,7 +22,7 @@ const CommandForm = ({
                     <Form.Control type="text" required placeholder="https://http.msging.net/commands" value={url} onChange={(e) => { setUrl(e.target.value) }} /><br />
                 </Col>
                 <Col sm="1">
-                    <a  target="_blank" rel="noopener noreferrer" href="https://help.blip.ai/hc/pt-br/articles/360058712774-Como-encontrar-a-API-KEY-do-meu-bot-">
+                    <a  target="_blank" rel="noopener noreferrer" href="https://forum.blip.ai/t/preciso-de-ajuda-com-blip-close-multiple-tickets/8634">
                         <FiHelpCircle />
                     </a>
                 </Col>
